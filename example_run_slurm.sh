43ab4b2d8e6175abeca4b338d4644a63d62e0750
Gohar Ali Siddiqui <gohar.siddiqui@tum.de>
2024-04-16 15:50:26 +0200

    wom machine module added

--- example_run_slurm.sh
+++ example_run_slurm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -J CE_Training  
+#SBATCH -J CE_Training
 ### number of cores
-#SBATCH -c 16
+#SBATCH -c 4
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -p gpucloud
+#SBATCH -p wom
 ##SBATCH -B 1:8:2
-#SBATCH --mem=30G
-##SBATCH --gpus=1
-#SBATCH --gres=shard:4
+#SBATCH --mem=20G
+#SBATCH --gpus=1
+##SBATCH --gres=shard:4
 #SBATCH --time=100:00:00
 #SBATCH --export=ALL
 #SBATCH -o /home/ge45daw/slurm_logs/slurm-%J.out
@@ -29,17 +29,25 @@ if [ "$SLURM_JOB_PARTITION" == "gpucloud" ] ; then
     watch -n 1 "nvidia-smi -q -d UTILIZATION >> /home/ge45daw/slurm_logs/slurm-$SLURM_JOB_ID.gpu" &> /dev/null &
     module load spack_skylake_avx512
     spack env activate ml_skylake_avx512
+elif [ "$SLURM_JOB_PARTITION" == "wom" ] ; then
+    ## WOM
+    watch -n 1 "nvidia-smi -q -d UTILIZATION >> /home/ge45daw/slurm_logs/slurm-$SLURM_JOB_ID.gpu" &> /dev/null &
+    module load spack_x86_64_v3
+    module load python/3.9-torch2-cuda12
 elif [ "$SLURM_JOB_PARTITION" == "carlos" ] ; then
     ## CARLOS
     watch -n 1 "nvidia-smi -q -d UTILIZATION >> /home/ge45daw/slurm_logs/slurm-$SLURM_JOB_ID.gpu" &> /dev/null &
-    module load spack_x86-64-2
+    module load spack_x86_64_v3
     spack env activate python-pytorch_haswell
+elif [ "$SLURM_JOB_PARTITION" == "microcloud" ] ; then
+    ## CARLOS
+    module load spack_x86_64_v3
+    module load python/3.9-torch1.11-cuda
 elif [ "$SLURM_JOB_PARTITION" == "zencloud" ] ; then
     ## ZENCLOUD
     echo "No ML env for spack implemented for zencloud"
 fi
 ########################################
-
 
 
 
