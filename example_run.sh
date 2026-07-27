8b9686e6c5b125d5b51e1d1c2fb60548e6137db2
Gohar Ali Siddiqui <gohar.siddiqui@tum.de>
2026-07-27 18:23:12 +0200

    Bug fixes.

--- example_run.sh
+++ example_run.sh
@@ -9,7 +9,7 @@ if [ ! -d ".venv" ]; then
     fi
     python -m venv .venv
     source .venv/bin/activate
-    pip install -r requirements.txt
+    pip install -e .
 else
     source .venv/bin/activate
 fi
